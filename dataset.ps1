8d62779d33a436f1ea13e687db902434b79bee15
dqnx19 <dan.k.19@outlook.com>
2026-08-25 18:57:20 +0200

    added dataset and script to build resource pack zip archives

---
+++ dataset.ps1
@@ -0,0 +1,182 @@
+$files = @(
+    @{
+        source = "creative-inventory.player-inventory.png"
+        target = @(
+            # 26
+            "26.2/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "26.1.2/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "26.1.1/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "26.1/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            
+            # 1.21
+            "1.21/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.1/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.2/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.3/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.4/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.5/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.6/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.7/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.8/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.9/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.10/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+            "1.21.11/assets/minecraft/textures/gui/container/creative_inventory/tab_inventory.png"
+        )
+    },
+    @{
+        source = "creative-inventory.search.png"
+        target = @(
+            # 26
+            "26.2/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "26.1.2/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "26.1.1/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "26.1/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+
+            # 1.21
+            "1.21/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.1/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.2/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.3/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.4/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.5/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.6/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.7/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.8/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.9/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.10/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+            "1.21.11/assets/minecraft/textures/gui/container/creative_inventory/tab_item_search.png"
+        )
+    },
+    @{
+        source = "creative-inventory.items.png"
+        target = @(
+            # 26
+            "26.2/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "26.1.2/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "26.1.1/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "26.1/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            
+            # 1.21
+            "1.21/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.1/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.2/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.3/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.4/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.5/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.6/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.7/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.8/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.9/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.10/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+            "1.21.11/assets/minecraft/textures/gui/container/creative_inventory/tab_items.png"
+        )
+    }
+)
+
+$versions = @(
+    @{
+        version     = "1.21"
+        pack_format = 48
+    },
+    @{
+        version     = "1.21.1"
+        pack_format = 48
+    },
+    @{
+        version     = "1.21.2"
+        pack_format = 57
+    },
+    @{
+        version     = "1.21.3"
+        pack_format = 57
+    },
+    @{
+        version     = "1.21.4"
+        pack_format = 61
+    },
+    @{
+        version     = "1.21.5"
+        pack_format = 71
+    },
+    @{
+        version     = "1.21.6"
+        pack_format = 80
+    },
+    @{
+        version     = "1.21.7"
+        pack_format = 81
+    },
+    @{
+        version     = "1.21.8"
+        pack_format = 81
+    },
+    @{
+        version     = "1.21.9"
+        pack_format = 88
+    },
+    @{
+        version     = "1.21.10"
+        pack_format = 88
+    },
+    @{
+        version     = "1.21.11"
+        pack_format = 94.1
+    },
+    @{
+        version     = "26.1"
+        pack_format = 101.1
+    },
+    @{
+        version     = "26.1.1"
+        pack_format = 101.1
+    },
+    @{
+        version     = "26.1.2"
+        pack_format = 101.1
+    },
+    @{
+        version     = "26.2"
+        pack_format = 107.1
+    }
+)
+
+foreach ($file in $files) {
+    foreach ($target in $file.target) {
+        $destination = "build/$target"
+
+        New-Item -ItemType Directory -Path (Split-Path $destination) -Force | Out-Null
+
+        Copy-Item "src/$($file.source)" $destination -Force
+    }
+}
+
+foreach ($version in $versions) {
+    $versionPath = "build/$($version.version)"
+    $packPath = "$versionPath/pack.mcmeta"
+
+    New-Item -ItemType Directory -Path $versionPath -Force | Out-Null
+
+    $mcmeta = @"
+{
+    "pack": {
+        "pack_format": $($version.pack_format),
+        "description": "Transparent GUI"
+    }
+}
+"@
+
+    Set-Content -Path $packPath -Value $mcmeta -Encoding utf8
+
+    # Vytvoření ZIPu
+    $zipPath = "result/glass-ui-$($version.version).zip"
+
+    New-Item -ItemType Directory -Path "result" -Force | Out-Null
+
+    Compress-Archive `
+        -Path "$versionPath/*" `
+        -DestinationPath $zipPath `
+        -Force
+}
+
+# Po dokončení můžeme build složku smazat
+Remove-Item "build" -Recurse -Force
